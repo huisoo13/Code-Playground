2fb903dbc5ec41bd90fdc2ca2a334ee6442bffdb
huisoo13 <wwweeey@gmail.com>
2025-04-22 12:09:51 +0900

    add swift-strategy-pattern project

---
+++ swift-strategy-pattern/swift-strategy-pattern/Scene/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-strategy-pattern
+//
+//  Created by 정희수 on 4/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ swift-strategy-pattern/swift-strategy-pattern/Source/AppError.swift
@@ -0,0 +1,53 @@
+//
+//  AppError.swift
+//  swift-strategy-pattern
+//
+//  Created by 정희수 on 4/21/25.
+//
+
+import UIKit
+
+enum AppError: Error {
+    case network
+    case invalidInput(String)
+    case unauthorized
+    case unknown
+}
+
+// MARK: - Strategy
+
+protocol ErrorHandlingStrategy {
+    func handle(_ error: AppError)
+}
+
+final class AlertErrorHandler: ErrorHandlingStrategy {
+    weak var target: UIViewController?
+    init(target: UIViewController? = nil) {
+        self.target = target
+    }
+    
+    func handle(_ error: AppError) {
+        let message: String
+
+        switch error {
+        case .network:
+            message = "인터넷 연결이 불안정합니다."
+        case .invalidInput(let reason):
+            message = reason
+        case .unauthorized:
+            message = "세션이 만료되었습니다."
+        case .unknown:
+            message = "알 수 없는 오류가 발생했습니다."
+        }
+
+        let alert = UIAlertController(title: "오류", message: message, preferredStyle: .alert)
+        alert.addAction(.init(title: "확인", style: .default))
+        target?.present(alert, animated: true)
+    }
+}
+
+final class LoggingErrorHandler: ErrorHandlingStrategy {
+    func handle(_ error: AppError) {
+        print("Error: \(error)")
+    }
+}
